071c55a7174ee10b682f6f121de1e7089d4cd8ab
hongjungin <94633439+hongjungin@users.noreply.github.com>
2025-02-11 20:03:13 +0900

    [Silver III] Title: 모든 순열, Time: 1816 ms, Memory: 63656 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.java
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -1,15 +1,15 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     static int N;
-    static int R;
     static int[] nums;
     static boolean[] visited;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        N = sc.nextInt();
-        R = N;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
         nums = new int[N];
         visited = new boolean[N];
 
@@ -17,7 +17,7 @@ public class Main {
     }
 
     static void perm(int depth) {
-        if (R == depth) {
+        if (depth == N) {
             for (int i = 0; i < N; i++) {
                 System.out.print(nums[i] + " ");
             }
