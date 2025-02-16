fcf5b5041be0d5518b10b16d621d9bd6141db005
hongjungin <jungini0000@gmail.com>
2025-02-16 09:47:47 +0900

    2/10 알고리즘 공부

---
+++ baekjoon_practice/B10597.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B10597 {
+    static int N;
+    static String input;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        input = br.readLine();
+        visited = new boolean[];
+
+        subset(0,0);
+    }
+
+    private static void subset(int depth, int start) {
+        if (depth == N) {
+            return;
+        }
+        // 전부 다 한 자리
+        // 2자리 수가 섞이면 1~9갯수 빼고 나누기 2하면 2자리인 개수 나옴.
+        if (input.length() < 9){
+
+        }
+
+    }
+}
--- baekjoon_practice/B31403.java
+++ baekjoon_practice/B31403.java
@@ -1,2 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.Callable;
+
 public class B31403 {
+    // 수로 생각했을 때
+    // 문자열로 생각했을 때
+    static int A;
+    static int B;
+    static int C;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        A = Integer.parseInt(br.readLine());
+        B = Integer.parseInt(br.readLine());
+        C = Integer.parseInt(br.readLine());
+        String s = "" + A + B;
+        int S = Integer.parseInt(s);
+
+        System.out.println(A + B - C);
+        System.out.println(S - C);
+    }
 }
