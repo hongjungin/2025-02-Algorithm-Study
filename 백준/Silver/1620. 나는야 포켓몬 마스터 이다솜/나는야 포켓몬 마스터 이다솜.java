839dd98230cdf6c45a8fca85cfb5bb525af22f3f
hongjungin <94633439+hongjungin@users.noreply.github.com>
2025-02-23 15:18:27 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 420 ms, Memory: 53616 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -4,37 +4,31 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken()); // 포켓몬 개수
-        int M = Integer.parseInt(st.nextToken()); // 문제 개수
+        int N = Integer.parseInt(st.nextToken()); 
+        int M = Integer.parseInt(st.nextToken()); 
 
         HashMap<String, Integer> nameToNum = new HashMap<>();
         ArrayList<String> numToName = new ArrayList<>(N + 1);
-
-        numToName.add(""); // 0번 인덱스는 사용하지 않음 (1부터 시작)
+        numToName.add(""); 
 
         for (int i = 1; i <= N; i++) {
             String name = br.readLine();
-            nameToNum.put(name, i); // 포켓몬 이름 → 번호 매핑
-            numToName.add(name);    // 번호 → 포켓몬 이름 매핑
+            nameToNum.put(name, i);  
+            numToName.add(name);    
         }
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < M; i++) {
             String query = br.readLine();
             if (Character.isDigit(query.charAt(0))) {
-                // 숫자인 경우 → 번호 → 이름 조회
-                int num = Integer.parseInt(query);
-                bw.write(numToName.get(num) + "\n");
+                sb.append(numToName.get(Integer.parseInt(query))).append("\n");
             } else {
-                // 문자열인 경우 → 이름 → 번호 조회
-                bw.write(nameToNum.get(query) + "\n");
+                sb.append(nameToNum.get(query)).append("\n");
             }
         }
 
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.print(sb); 
     }
 }
