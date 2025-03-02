eb995229fe437814db572b2fbc6be9e57cb569c6
hongjungin <94633439+hongjungin@users.noreply.github.com>
2025-03-03 02:56:48 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char ch = br.readLine().charAt(0);
+        System.out.println((int) ch);
+    }
+}
