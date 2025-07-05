ab72fe80e848b3330f9e2e00725faf4d14c0d350
hongjungin <jungini0000@gmail.com>
2025-07-05 15:32:58 +0900

    7/5 : 알고리즘 공부

--- String-문자열/Main25.java
+++ String-문자열/Main25.java
@@ -1,18 +1,24 @@
-//import java.util.Scanner;
-//
-//class Main2 {
-//    public String solution(String str){
-//        String answer = "";
-//
-//
-//        return answer;
-//    }
-//
-//    public static void main(String[] args) {
-//        Main2 T = new Main2();
-//        Scanner kb = new Scanner(System.in);
-//        String str = kb.next();
-//        System.out.println(T.solution(str));
-//    }
-//
-//}
+import java.util.Scanner;
+
+class Main25 {
+    public int solution(String str){
+        int answer = 0;
+        StringBuilder sb = new StringBuilder();
+        char[] arr = str.toCharArray();
+        for(int i = 0; i < arr.length; i++){
+            if(Character.isDigit(arr[i])){
+                sb.append(arr[i]);
+            }
+        }
+        answer = Integer.parseInt(sb.toString());
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main25 T = new Main25();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(T.solution(str));
+    }
+
+}
