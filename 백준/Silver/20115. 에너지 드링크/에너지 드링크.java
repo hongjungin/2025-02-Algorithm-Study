4e159fb8edb6c20e0ab4a99a4d303d949744b97e
hongjungin <94633439+hongjungin@users.noreply.github.com>
2025-11-11 22:16:32 +0900

    [Silver III] Title: 에너지 드링크, Time: 272 ms, Memory: 32264 KB -BaekjoonHub

---
+++ 백준/Silver/20115. 에너지 드링크/에너지 드링크.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+// 가장 큰 애 말고 다 2로 나눠
+public class Main {
+    static int[] drinks;
+    static double total;
+    static int N;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        drinks = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            drinks[i] = Integer.parseInt(st.nextToken());
+        }
+
+        total = 0;
+
+        Arrays.sort(drinks);
+
+        for (int i = 0; i < N - 1; i++) {
+            total += (double)drinks[i] / 2;
+        }
+
+        total += drinks[N - 1];
+
+        System.out.println(total);
+
+    }
+
+}
