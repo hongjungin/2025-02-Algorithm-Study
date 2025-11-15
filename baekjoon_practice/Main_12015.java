e4b867855e5c0dd6b75a7287d697b05d220f5f3a
hongjungin <jungini0000@gmail.com>
2025-11-15 18:02:51 +0900

    11/15 : 알고리즘 공부

---
+++ baekjoon_practice/Main_12015.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+
+public class Main_12015 {
+
+    public static void main (String[] args) {
+
+    }
+}
---
+++ baekjoon_practice/Main_4963.java
@@ -0,0 +1,79 @@
+import java.util.*;
+import java.io.*;
+
+// 대각선 까지 가능
+
+public class Main_4963 {
+
+    static int w, h;
+    static int[][] map;
+    static boolean[][] visited;
+    static int[] dx = {-1, -1, -1, 0, 1, 1, 1, 0};
+    static int[] dy = {-1, 0, 1, 1, 1, 0, -1, -1};
+
+    public static void main (String[] args) throws Exception {
+
+        StringBuilder sb = new StringBuilder();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String line = br.readLine();
+            StringTokenizer st = new StringTokenizer(line);
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
+
+            if (w == 0 && h == 0) break;
+
+            map = new int[h][w];
+
+            for (int i = 0; i < h; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < w; j++) {
+
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            visited = new boolean[h][w];
+
+            int count = 0;
+
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    if (!visited[i][j] && map[i][j] == 1) {
+                        bfs(i, j);
+                        count++;
+                    }
+                }
+            }
+
+            sb.append(count).append("\n");
+        }
+
+        System.out.println(sb);
+
+    }
+
+    static void bfs (int startX, int startY) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{startX, startY});
+        visited[startX][startY] = true;
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int cx = cur[0];
+            int cy = cur[1];
+
+            for (int i = 0; i < 8; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if (nx >= 0 && nx < h && ny >= 0 && ny < w && !visited[nx][ny] && map[nx][ny] == 1) {
+                    queue.offer(new int[]{nx, ny});
+                    visited[nx][ny] = true;
+                }
+            }
+        }
+    }
+}
