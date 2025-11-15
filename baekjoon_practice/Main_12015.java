dc9e9254304bbf6ac09446c2f656d9cef23e72a3
hongjungin <jungini0000@gmail.com>
2025-11-15 21:25:38 +0900

    11/15 : 알고리즘 공부

--- baekjoon_practice/Main_12015.java
+++ baekjoon_practice/Main_12015.java
@@ -3,7 +3,42 @@ import java.io.*;
 
 public class Main_12015 {
 
-    public static void main (String[] args) {
+    static int N;
+    static int[] arr;
+    static List<Integer> lis;
+
+    public static void main (String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        lis = new ArrayList<>();
+        lis.add(arr[0]);
+
+        for (int i = 1; i < N; i++) {
+
+            if (arr[i] > lis.get(lis.size()-1)) {
+                lis.add(arr[i]);
+            } else {
+
+                int pos = Collections.binarySearch(lis, arr[i]);
+
+                if (pos < 0) pos = - pos - 1;
+                lis.set(pos, arr[i]);
+
+            }
+        }
+
+        System.out.println(lis.size());
+
 
     }
 }
