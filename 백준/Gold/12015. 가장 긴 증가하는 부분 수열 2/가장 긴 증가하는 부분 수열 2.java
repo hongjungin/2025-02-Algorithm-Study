3b77be6c1fbeb40a7dc163af5e404a73726f13ac
hongjungin <94633439+hongjungin@users.noreply.github.com>
2025-11-15 21:23:38 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 568 ms, Memory: 150472 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int N;
+    static int[] arr;
+    static List<Integer> lis;
+
+    public static void main (String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        arr = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        lis = new ArrayList<>();
+        lis.add(arr[0]);
+
+        for (int i = 1; i < N; i++) {
+
+            if (arr[i] > lis.get(lis.size()-1)) {
+                lis.add(arr[i]);
+            } else {
+
+                int pos = Collections.binarySearch(lis, arr[i]);
+
+                if (pos < 0) pos = - pos - 1;
+                lis.set(pos, arr[i]);
+
+            }
+        }
+
+        System.out.println(lis.size());
+
+
+    }
+}
