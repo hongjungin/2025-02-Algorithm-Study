a2b6da904fa7503260e623d8f521f8637d543f5f
hongmandu <94633439+hongjungin@users.noreply.github.com>
2026-04-25 17:33:46 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 68 ms, Memory: 12832 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.java
@@ -1,76 +1,65 @@
-import java.util.*;
 import java.io.*;
-import java.lang.*;
-
-// 수는 위치 고정.
-// 연산자가 순열 조합 -> 연산자를 배치하고 그 다음에 연산하고 그다음에 최대최소?
+import java.util.*;
 
 public class Main {
 
     static int N;
-    static int[] arr;
-    static int[] operator; // + - * /
-    static int[] selected;
-    static int max = Integer.MIN_VALUE;
-    static int min = Integer.MAX_VALUE;
+    static int[] operator;
+    static int[] input;
+    static int min, max;
 
-    public static void main (String args[]) throws IOException {
-
+    public static void main (String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        arr = new int[N];
+        input = new int[N];
         operator = new int[4];
-        selected = new int[N-1];
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            int num = Integer.parseInt(st.nextToken());
+            input[i] = num;
         }
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 4; i++) {
-            operator[i] = Integer.parseInt(st.nextToken());
+            int op = Integer.parseInt(st.nextToken());
+            operator[i] = op;
         }
 
-        // 이제 배치
-        dfs(0, arr[0]);
-        System.out.println(max);
-        System.out.println(min);
+        min = Integer.MAX_VALUE;
+        max = Integer.MIN_VALUE;
+
+        dfs(0, input[0]);
+        StringBuilder sb = new StringBuilder();
+        sb.append(max).append("\n").append(min);
+        System.out.println(sb);
     }
 
-    static void dfs (int depth, int next) {
+    private static void dfs(int depth, int cal) {
 
         if (depth == N - 1) {
-            min = Math.min(min, next);
-            max = Math.max(max, next);
+            min = Math.min(min, cal);
+            max = Math.max(max, cal);
             return;
         }
-
-        int val = 0;
 
         for (int i = 0; i < 4; i++) {
             if (operator[i] > 0) {
                 operator[i]--;
-                val = cal(next, arr[depth + 1], i);
-                dfs(depth + 1, val);
+                if (i == 0) {
+                    dfs(depth + 1, cal + input[depth+1]);
+                } else if (i == 1) {
+                    dfs(depth + 1, cal - input[depth+1]);
+                } else if (i == 2) {
+                    dfs(depth + 1, cal * input[depth+1]);
+                } else {
+                    dfs(depth + 1, cal / input[depth+1]);
+                }
+
                 operator[i]++;
             }
         }
-    }
 
-    private static int cal(int cur, int next, int operator) {
-
-        if (operator == 0) {
-            return cur + next;
-
-        } else if (operator == 1) {
-            return cur - next;
-        } else if (operator == 2) {
-            return cur * next;
-        } else if (operator == 3) {
-            return cur / next;
-        }
-        return 0;
     }
 }
